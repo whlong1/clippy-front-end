a3a6af60cb996a76ba870023ceadcee83a9da5e4
whlong1 <whunterlong@gmail.com>
2022-12-11 19:58:00 -0500

    Add notes and msg useState variable

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useAuth0 } from "@auth0/auth0-react"
 import NavBar from './components/NavBar/NavBar'
 import * as authService from './services/authService'
@@ -8,13 +8,27 @@ import LoginButton from './components/LoginButton/LoginButton'
 import LogoutButton from './components/LogoutButton/LogoutButton'
 
 const App = () => {
+  const [msg, setMsg] = useState('')
   const { user, getAccessTokenSilently } = useAuth0()
 
   // Does the domain, audience, and scope matter?
+  // This information is included on the back end (middleware/auth.js), so 
+  // presumably the client and api apps registered in auth0 share an audience.
+
+  // Alternate getAccessTokenSilently
   // const domain = "dev-56chijhh78c0pcex.us.auth0.com"
   // const token = await getAccessTokenSilently({
   //   audience: `https://${domain}`, scope: "read:current_user"
   // })
+
+  // Notes:
+  // If token exists, our back end controller will retrieve a profile 
+  // associated with the user sub property decoded from the jwt.
+
+  // If a token does not exist, and the user is signing up,
+  // we redirect them to a 'complete your profile page'
+  // The back end controller needs to create a new profile instance,
+  // associated with that sub property, and return it to the client.
 
   console.log('auth0 User Data:', user)
 
@@ -25,8 +39,9 @@ const App = () => {
       if (token) {
         const profileData = await authService.getUserMetadata(token)
         console.log('Profile Data', profileData)
+        setMsg('Welcome back to Clippy!')
       } else {
-        console.log('Please sign up!')
+        setMsg('New to Clippy? Click here to create an account.')
       }
     }
     getToken()
@@ -35,6 +50,9 @@ const App = () => {
   return (
     <>
       <NavBar />
+      <p>
+        {msg}
+      </p>
       <LoginButton />
       <LogoutButton />
     </>
