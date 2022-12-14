7df4934ab565cfbf019b39c03ff67042d0f9965a
whlong1 <whunterlong@gmail.com>
2022-12-14 10:06:20 -0500

    Remove unused components

--- src/components/Router/Router.jsx
+++ src/components/Router/Router.jsx
@@ -1,36 +1,17 @@
 import { Routes, Route } from 'react-router-dom'
 
-import Signup from '../../pages/Signup/Signup'
 import Landing from '../../pages/Landing/Landing'
-import Profiles from '../../pages/Profiles/Profiles'
 import AdminPanel from '../../pages/AdminPanel/AdminPanel'
 
-import ProtectedRoute from '../ProtectedRoute/ProtectedRoute'
+// import ProtectedRoute from '../ProtectedRoute/ProtectedRoute'
 
 const Router = () => {
   return (
     <Routes>
       <Route path="/" element={<Landing />} />
       <Route
-        path="/signup"
-        element={<Signup/>}
-      />
-      <Route
-        path="/profiles"
-        element={
-          // <ProtectedRoute>
-          <Profiles />
-          // </ProtectedRoute>
-        }
-      />
-
-      <Route
         path="/admin"
-        element={
-          // <ProtectedRoute>
-          <AdminPanel/>
-          // </ProtectedRoute>
-        }
+        element={<AdminPanel />}
       />
     </Routes>
   )
--- src/components/SignupButton/SignupButton.jsx
+++ src/components/SignupButton/SignupButton.jsx
@@ -1,6 +1,3 @@
-// { screen_hint: 'signup' }
-
-import React from "react"
 import { useAuth0 } from "@auth0/auth0-react"
 
 const SignupButton = () => {
