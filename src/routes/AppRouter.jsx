90f731842aca9e485c5ef802cc6c9c94249ea0bf
whlong1 <whunterlong@gmail.com>
2023-01-01 11:15:39 -0500

    Remove Onboarding from AppRouter

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,24 +1,22 @@
 import { Routes, Route } from 'react-router-dom'
 
-import Landing from '../pages/Landing/Landing'
 import AdminPanel from '../pages/AdminPanel/AdminPanel'
 
 import PeopleRouter from './PeopleRouter'
 import AttendanceRouter from './AttendanceRouter'
 import DeliverablesRouter from './DeliverablesRouter'
-import Onboarding from '../pages/Onboarding/Onboarding'
 
 const AppRouter = () => {
   return (
     <Routes>
-      <Route path="/" element={<Landing />} />
+      <Route path="/" element={<h1>Welcome back</h1>} />
       <Route path="/admin" element={<AdminPanel />} />
       <Route path="/*" element={<h1>Error Page</h1>} />
-      <Route path="/onboarding" element={<Onboarding />} />
 
       <Route path='/people/*' element={<PeopleRouter />} />
       <Route path='/attendance/*' element={<AttendanceRouter />} />
       <Route path='/deliverables/*' element={<DeliverablesRouter />} />
+
     </Routes>
   )
 }
