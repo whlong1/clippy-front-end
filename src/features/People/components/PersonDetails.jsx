3e45b3e60cd84cfbf2026d8da844ba435dba793d
whlong1 <whunterlong@gmail.com>
2023-01-02 22:37:38 -0500

    Build out PersonDetails component

--- src/features/People/components/PersonDetails.jsx
+++ src/features/People/components/PersonDetails.jsx
@@ -1,13 +1,32 @@
-import { useParams } from "react-router-dom"
+import { useParams, useLocation } from "react-router-dom"
 import { useProfileDetails } from "../../../hooks/useProfileDetails"
 
 const PersonDetails = () => {
+  const location = useLocation()
   const { profileId } = useParams()
   const { profile, status } = useProfileDetails(profileId)
+  const cohortRole = location.search.slice(6, -1).toUpperCase()
+
+  if (status === 'error') return <h1>Error</h1>
+  if (status === 'loading') return <h1>Loading...</h1>
 
   return (
     <section>
-      People details
+      <img
+        alt="github-profile"
+        style={{ width: '50px' }}
+        src={`https://github.com/${profile.gitHubUserName}.png`}
+      />
+
+      {cohortRole}
+      {profile.email}
+      {profile.lastName}
+      {profile.firstName}
+      {profile.preferredName}
+      {profile.gitHubUserName}
+      {profile.linkedInUserName}
+      {profile.codeWarsUserName}
+      {profile.preferredPronouns}
     </section>
   )
 }
