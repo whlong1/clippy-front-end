947f12a67ba3fa0c8698495c29af3f0fd280d909
whlong1 <whunterlong@gmail.com>
2023-01-03 23:57:17 -0500

    Build out remaining attendance service functions

--- src/services/attendanceService.js
+++ src/services/attendanceService.js
@@ -26,8 +26,48 @@ const createAttendance = async (data) => {
 }
 
 
+const updateAttendance = async (data) => {
+  const { attendanceId } = data
+  try {
+    const res = await fetch(`${BASE_URL}/${attendanceId}`,
+      {
+        method: 'PUT',
+        headers: {
+          'Authorization': `Bearer ${tokenService.getToken()}`,
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+      })
+    return await res.json()
+  } catch (error) {
+    throw error
+  }
+}
+
+
+
+const deleteAttendance = async (data) => {
+  const { attendanceId } = data
+  try {
+    const res = await fetch(`${BASE_URL}/${attendanceId}`,
+      {
+        method: 'DELETE',
+        headers: {
+          'Authorization': `Bearer ${tokenService.getToken()}`,
+        },
+      })
+    return await res.json()
+  } catch (error) {
+    throw error
+  }
+}
+
+
+
 
 export {
   indexAttendance,
   createAttendance,
+  updateAttendance,
+  deleteAttendance,
 }
