b617038a703d7146abd2b152d51661e6f79b3170
whlong1 <whunterlong@gmail.com>
2023-01-04 14:11:20 -0500

    Build out deliverable details component

---
+++ src/features/Deliverables/DeliverableDetails.jsx
@@ -0,0 +1,23 @@
+import { useParams } from 'react-router-dom'
+
+// Hooks
+import { useDeliverableDetails } from '../../hooks/useDeliverableDetails'
+
+const DeliverableDetails = (props) => {
+  // const { user, cohortId } = props
+  const { deliverableId } = useParams()
+  const { deliverableDetails, status } = useDeliverableDetails(deliverableId)
+
+  console.log('Deliverable details', deliverableDetails)
+
+  if (status === 'error') return <h1>Error</h1>
+  if (status === 'loading') return <h1>Loading...</h1>
+
+  return (
+    <section>
+      Details
+    </section>
+  )
+}
+
+export default DeliverableDetails 
