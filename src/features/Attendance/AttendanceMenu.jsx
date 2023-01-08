575d8635cf12083b04881653df4f4bdca7d5440a
whlong1 <whunterlong@gmail.com>
2023-01-07 21:17:57 -0500

    Add conditional rendering to AttendanceMenu for admin and student, create MyAttendance component

--- src/features/Attendance/AttendanceMenu.jsx
+++ src/features/Attendance/AttendanceMenu.jsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom'
 
 // Components
+import MyAttendance from './MyAttendance'
 import AttendanceList from './AttendanceList'
 import MenuLayout from '../../layouts/MenuLayout'
 
@@ -12,17 +13,22 @@ const AttendanceMenu = (props) => {
   if (status === 'error') return <h1>Error</h1>
   if (status === 'loading') return <h1>Loading...</h1>
 
+  const adminContent = (
+    <>
+      <Link to='/attendance/new'><button>New Attendance</button></Link>
+      <AttendanceList attendance={attendance} />
+    </>
+  )
+
   return (
     <MenuLayout {...props}>
       <p>Attendance Menu</p>
 
-      {props.user.isAdmin &&
-        <Link to='/attendance/new'>
-          <button>New Attendance</button>
-        </Link>
+      {props.user.isAdmin
+        ? { adminContent }
+        : <MyAttendance attendance={attendance} />
       }
 
-      <AttendanceList attendance={attendance} />
     </MenuLayout>
   )
 }
---
+++ src/features/Attendance/MyAttendance.jsx
@@ -0,0 +1,10 @@
+const MyAttendance = ({ attendance }) => {
+  console.log(attendance)
+  return (
+    <>
+      MyAttendance
+    </>
+  )
+}
+
+export default MyAttendance 
