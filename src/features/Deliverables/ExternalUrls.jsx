c0b200d0064f0f21f19ba3d82d0bbdc66a13319e
whlong1 <whunterlong@gmail.com>
2023-01-10 00:15:46 -0500

    Build out ExternalUrls component

---
+++ src/features/Deliverables/ExternalUrls.jsx
@@ -0,0 +1,24 @@
+const ExternalUrls = ({ student }) => {
+  // Might be worth handling bad links in here
+  const urlTable = {
+    miscUrl: { text: 'Misc URL', link: student.miscUrl },
+    trelloUrl: { text: 'Trello', link: student.trelloUrl },
+    gitHubUrl: { text: 'GitHub', link: student.gitHubUrl },
+    deploymentUrl: { text: 'Deployment', link: student.deploymentUrl },
+    codeSandboxUrl: { text: 'Code Sandbox', link: student.codeSandboxUrl },
+  }
+
+  const filteredUrls = Object.keys(urlTable).filter((url) => student.hasOwnProperty(url))
+
+  if (!filteredUrls.length) return <p>N/A</p>
+
+  return (
+    filteredUrls.map((url, idx) => (
+      <a key={idx} target="_blank" rel="noreferrer" href={urlTable[url].link}>
+        {urlTable[url].text}
+      </a>
+    ))
+  )
+}
+
+export default ExternalUrls
--- src/features/Deliverables/StudentDeliverableRow.jsx
+++ src/features/Deliverables/StudentDeliverableRow.jsx
@@ -1,18 +1,25 @@
 import { Link } from "react-router-dom"
+
+// Components
+import ExternalUrls from "./ExternalUrls"
 import ProfileInfo from "../../components/ProfileInfo/ProfileInfo"
 
 const StudentDeliverableRow = ({ deliverableId, student }) => {
   const { _id: studentDeliverableId } = student
   const path = `/deliverables/${deliverableId}/students/${studentDeliverableId}/grade`
 
-  // console.log(student)
   return (
-    <Link to={path}>
-      <p>Status: {student.status}</p>
-      <ProfileInfo profile={student} />
-      <p>{student.preferredName}</p>
-      <p>{student.lastName}</p>
-    </Link>
+    <div>
+      <Link to={path}>
+        <p>Status: {student.status}</p>
+        <ProfileInfo profile={student} />
+        <p>{student.preferredName}</p>
+        <p>{student.lastName}</p>
+      </Link>
+
+      <ExternalUrls student={student}/>
+      
+    </div>
   )
 }
 
