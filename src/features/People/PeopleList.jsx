2a4d11270334a956eed01609adce432aa5c3cd79
whlong1 <whunterlong@gmail.com>
2023-03-03 14:42:12 -0500

    Add role section header, fix scrollbar css

--- src/features/People/PeopleList.jsx
+++ src/features/People/PeopleList.jsx
@@ -3,16 +3,23 @@ import ProfileInfo from '../../components/ProfileInfo/ProfileInfo'
 
 const PeopleList = (props) => {
   const { role, people } = props
+  const roleHeader = role[0].toUpperCase() + role.slice(1)
+
+
   return (
-    <>
-      <h2>{role}</h2>
-      <p>{people.length}</p>
+    <section>
+      <header>
+        <h2>{roleHeader}</h2>
+        <p>{people.length}</p>
+        <button>X</button>
+      </header>
+
       {people.map((profile) => (
         <Link key={profile._id} to={`/people/${profile._id}`}>
           <ProfileInfo profile={profile} />
         </Link>
       ))}
-    </>
+    </section>
   )
 }
 
