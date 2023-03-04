4ec6c4453c90262804e9939e5f07ba5cb1597da1
whlong1 <whunterlong@gmail.com>
2023-03-04 13:22:16 -0500

    Build out Copied! status effect

--- src/features/Deliverables/DeliverableHeader.jsx
+++ src/features/Deliverables/DeliverableHeader.jsx
@@ -1,8 +1,12 @@
+import { useState, useEffect } from 'react'
+
+// Components
+import DueDate from './DueDate'
 import RequirementsList from './RequirementsList'
 import SubmissionTracker from './SubmissionTracker'
-import DueDate from './DueDate'
 
 const DeliverableHeader = ({ deliverable, handleDelete }) => {
+  const [copied, setCopied] = useState(false)
 
   const statusTable = {
     missing: "Missing",
@@ -14,14 +18,26 @@ const DeliverableHeader = ({ deliverable, handleDelete }) => {
     statusTable[s.status] ? statusTable[s.status] + "\n" : "\n"
   ).join("")
 
+  useEffect(() => {
+    const resetCopy = () => {
+      setTimeout(() => setCopied(false), 2000)
+    }
+    if (copied) resetCopy()
+  }, [copied])
+
+  const handleCopy = () => {
+    setCopied(true)
+    navigator.clipboard.writeText(deliverableRecord)
+  }
+
   return (
     <header className="header">
 
       <section>
         <h1>{deliverable.name}</h1>
         <button onClick={handleDelete}>Delete</button>
-        <button onClick={() => navigator.clipboard.writeText(deliverableRecord)}>
-          Copy
+        <button onClick={handleCopy}>
+          {copied ? "Copied!" : "Copy"}
         </button>
       </section>
 
