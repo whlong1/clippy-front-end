6e5aa04be822ea18691373b1f9a4724e308e6272
whlong1 <whunterlong@gmail.com>
2023-03-10 11:12:05 -0500

    Style layout of DeleteConfirmation, add box shadow

--- src/features/Deliverables/DeleteConfirmation.jsx
+++ src/features/Deliverables/DeleteConfirmation.jsx
@@ -3,7 +3,10 @@ const DeleteConfirmation = ({ setIsOpen, handleDelete }) => {
   return (
     <div className="confirmation">
       <h1>Delete Deliverable</h1>
-      <p>Are you sure about this? This action cannot be undone. </p>
+      <p>
+        Are you sure?
+        This action cannot be undone.
+      </p>
       <section>
         <button onClick={handleDelete}>
           DELETE
