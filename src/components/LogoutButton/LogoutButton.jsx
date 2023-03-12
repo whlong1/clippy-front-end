0bb17e4dfe4578747625b2a93fc6768ce1868179
whlong1 <whunterlong@gmail.com>
2023-03-12 12:33:47 -0400

    Add nav svgs, refactor Nav component, move nav styling to new stylesheet

--- src/components/LogoutButton/LogoutButton.jsx
+++ src/components/LogoutButton/LogoutButton.jsx
@@ -1,11 +1,12 @@
 import { useAuth0 } from "@auth0/auth0-react"
+import logoutIcon from '../../assets/icons/nav/logout.svg'
 
 const LogoutButton = () => {
   const { logout } = useAuth0()
 
   return (
     <button onClick={() => logout({ returnTo: window.location.origin })}>
-      
+      <img src={logoutIcon} alt="" />
     </button>
   )
 }
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,5 +1,15 @@
 import { NavLink } from 'react-router-dom'
+import './Nav.css'
+
+// Components
 import LogoutButton from '../LogoutButton/LogoutButton'
+
+// Assets
+import admin from '../../assets/icons/nav/admin.svg'
+import people from '../../assets/icons/nav/people.svg'
+import profile from '../../assets/icons/nav/profile.svg'
+import attendance from '../../assets/icons/nav/attendance.svg'
+import deliverables from '../../assets/icons/nav/deliverables.svg'
 
 // Users can only access this nav after logging in.
 // The only conditional rendering we need here is for admin access.
@@ -10,13 +20,41 @@ const Nav = (props) => {
     <nav className='appNav'>
       <ul>
         <li><NavLink to="/"></NavLink></li>
-        <li><NavLink to="/people">P</NavLink></li>
-        <li><NavLink to="/attendance">A</NavLink></li>
-        <li><NavLink to="/deliverables">D</NavLink></li>
 
-        {isAdmin && <li><NavLink to="/admin">*</NavLink></li>}
-        <li><NavLink to="/profile">U</NavLink></li>
-        <li><LogoutButton /></li>
+        <li>
+          <NavLink to="/people">
+            <img src={people} alt="" />
+          </NavLink>
+        </li>
+
+        <li>
+          <NavLink to="/attendance">
+            <img src={attendance} alt="" />
+          </NavLink>
+        </li>
+
+
+        <li>
+          <NavLink to="/deliverables">
+            <img src={deliverables} alt="" />
+          </NavLink>
+        </li>
+
+        {isAdmin &&
+          <li>
+            <NavLink to="/admin">
+              <img src={admin} alt="" />
+            </NavLink>
+          </li>
+        }
+
+        <li>
+          <NavLink to="/profile">
+            <img src={profile} alt="" />
+          </NavLink>
+        </li>
+
+        <LogoutButton />
       </ul>
     </nav>
   )
