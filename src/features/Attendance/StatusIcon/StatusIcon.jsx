c564dda9bbcd6a9252a1615d81ed80a8e15a20a4
whlong1 <whunterlong@gmail.com>
2023-03-12 17:03:23 -0400

    Build out lookup object

--- src/features/Attendance/StatusIcon/StatusIcon.jsx
+++ src/features/Attendance/StatusIcon/StatusIcon.jsx
@@ -1,5 +1,27 @@
-const StatusIcon = () => {
-  return(
+// Assets
+import late from '../../../assets/icons/attendance/late.svg'
+import absent from '../../../assets/icons/attendance/absent.svg'
+import closed from '../../../assets/icons/attendance/closed.svg'
+import present from '../../../assets/icons/attendance/present.svg'
+import exception from '../../../assets/icons/attendance/exception.svg'
+import withdrawn from '../../../assets/icons/attendance/withdrawn.svg'
+
+// enum: ['P', 'A', 'L', 'W', 'EC', 'SC', 'H'],
+const StatusIcon = ({ status }) => {
+  console.log(status)
+  
+  const lookup = {
+    L: late,
+    A: absent,
+    SC: closed,
+    P: present,
+    W: withdrawn,
+    EC: exception,
+  }
+
+
+
+  return (
     <img src="" alt="" />
   )
 }
--- src/features/Attendance/StudentStatusRow.jsx
+++ src/features/Attendance/StudentStatusRow.jsx
@@ -1,10 +1,13 @@
+// Components
+import StatusIcon from "./StatusIcon/StatusIcon"
 import ProfileInfo from "../../components/ProfileInfo/ProfileInfo"
 
 const StudentStatusRow = ({ student }) => {
   // Add lookup table for status icons
   return (
     <div className="row">
-      <p>{student.status}</p>
+      {/* <p>{student.status}</p> */}
+      <StatusIcon status={student.status} />
       <ProfileInfo profile={student} />
     </div>
   )
