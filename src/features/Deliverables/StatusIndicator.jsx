cede0ee5265171bc8d976775a9884c28f8c86456
whlong1 <whunterlong@gmail.com>
2023-03-12 16:12:54 -0400

    Add lookup object and img to StatusIndicator

--- src/features/Deliverables/StatusIndicator.jsx
+++ src/features/Deliverables/StatusIndicator.jsx
@@ -1,20 +1,22 @@
 import missing from '../../assets/icons/status/missing.svg'
-import pending from '../../assets/icons/status/pending.svg'
 import assigned from '../../assets/icons/status/assigned.svg'
 import complete from '../../assets/icons/status/complete.svg'
+import pendingAudit from '../../assets/icons/status/pending.svg'
 import incomplete from '../../assets/icons/status/incomplete.svg'
 
+const StatusIndicator = ({ status }) => {
+  console.log(status)
 
+  const lookup = {
+    missing,
+    assigned,
+    complete,
+    incomplete,
+    pendingAudit,
+  }
 
-const StatusIndicator = ({status}) => {
-  console.log(
-    's', status
-  )
   return (
-    // <input type="checkbox" checked={status} readOnly />
-    <div>
-      a
-    </div>
+    <img src={lookup[status]} alt="" />
   )
 }
 
