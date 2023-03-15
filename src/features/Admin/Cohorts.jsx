6462da7a2c8bc02e0eaa96641f994759077b273b
whlong1 <whunterlong@gmail.com>
2023-03-14 23:40:29 -0400

    Add start and end dates to cohort list

--- src/features/Admin/Cohorts.jsx
+++ src/features/Admin/Cohorts.jsx
@@ -12,21 +12,23 @@ const Cohorts = () => {
   if (status === 'error') return <ContentStatus status={status} />
   if (status === 'loading') return <ContentStatus status={status} />
 
+  console.log(cohorts)
   return (
     <section>
       <h1>Cohorts</h1>
+      <Link to="/admin/cohorts/new">New Cohort</Link>
 
       <h2>Cohort List</h2>
       {cohorts.map((c) => (
         <div key={c._id}>
           <p>{c.name}</p>
+          <p>{c.startDate.slice(0, 10)} - {c.endDate.slice(0, 10)}</p>
           <Link to="/admin/cohorts/edit" state={c} >
             <button>Edit</button>
           </Link>
         </div>
       ))}
 
-      <Link to="/admin/cohorts/new">New Cohort</Link>
 
     </section>
   )
