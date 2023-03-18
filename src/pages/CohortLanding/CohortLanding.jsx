acd5b207a2f48b902f922b5b9df5c20544d453c9
whlong1 <whunterlong@gmail.com>
2023-03-18 17:35:24 -0400

    Add logo assets to landing, build out pickLogo function for testing

--- src/pages/CohortLanding/CohortLanding.jsx
+++ src/pages/CohortLanding/CohortLanding.jsx
@@ -1,17 +1,52 @@
+import "./CohortLanding.css"
+
 // Components
 import SelectCohort from "../../components/SelectCohort/SelectCohort"
 
 // Assets
-
+import PinkOnWhiteSq from "../../assets/logos/logotype-sq-p.svg"
+import WhiteOnPinkSq from "../../assets/logos/logotype-sq-w.svg"
+import WhiteOnPinkCirc from "../../assets/logos/logotype-ci-w.svg"
+import PinkOnWhiteCirc from "../../assets/logos/logotype-ci-p.svg"
+import BlueOnBlackSq from "../../assets/logos/logotype-sq-b-dark.svg"
+import BlueOnWhiteSq from "../../assets/logos/logotype-sq-b-light.svg"
+import BlueOnBlackCirc from "../../assets/logos/logotype-ci-b-dark.svg"
+import BlueOnWhiteCirc from "../../assets/logos/logotype-ci-b-light.svg"
 
 const CohortLanding = (props) => {
-  // Could include MenuLayout in here if we like the way that looks.
+
+  const picklogo = (num) => {
+    switch (num) {
+      case 1:
+        return <img src={PinkOnWhiteSq} alt="Abstract flamingo" />
+      case 2:
+        return <img src={WhiteOnPinkSq} alt="Abstract flamingo" />
+      case 3:
+        return <img src={BlueOnWhiteSq} alt="Abstract flamingo" />
+      case 4:
+        return <img src={BlueOnBlackSq} alt="Abstract flamingo" />
+      case 5:
+        return <img src={WhiteOnPinkCirc} alt="Abstract flamingo" />
+      case 6:
+        return <img src={PinkOnWhiteCirc} alt="Abstract flamingo" />
+      case 7:
+        return <img src={BlueOnWhiteCirc} alt="Abstract flamingo" />
+      case 8:
+        return <img src={BlueOnBlackCirc} alt="Abstract flamingo" />
+      default:
+        return <img src={PinkOnWhiteSq} alt="Abstract flamingo" />
+    }
+  }
 
   return (
-    <main className="page">
-      <h1>Clippy</h1>
-      Welcome back to...
-      <SelectCohort {...props} />
+    <main className="landing">
+      <header>
+        <h1>Welcome back to</h1>
+        <SelectCohort {...props} />
+      </header>
+      <section>
+        {picklogo(4)}
+      </section>
     </main>
   )
 }
