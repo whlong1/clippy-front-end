c95346edb7300a2d636263cf9f48918e12dc7900
whlong1 <whunterlong@gmail.com>
2023-03-23 11:07:06 -0400

    Update checks performed by isValidUrl helper, conditionally render broken link icon

--- src/features/Deliverables/components/ExternalUrls/ExternalUrls.jsx
+++ src/features/Deliverables/components/ExternalUrls/ExternalUrls.jsx
@@ -2,6 +2,7 @@ import './ExternalUrls.css'
 
 // Assets
 import share from '../../../../assets/icons/share.svg'
+import broken from '../../../../assets/icons/broken.svg'
 
 // Components
 import ExternalLink from '../../../../components/ExternalLink/ExternalLink'
@@ -18,14 +19,20 @@ const ExternalUrls = ({ student }) => {
   const filteredUrls = Object.keys(urlTable).filter((url) => student.hasOwnProperty(url))
 
   // Add visual indicator to link if URL is invalid?
-  // const isValidUrl = (url) => {
-  //   const regex = /^(http|https):\/\//
-  //   return regex.test(url)
-  // }
+  const isValidUrl = (url) => {
+    const regex = /^(http|https):\/\//
+    if (!url) return false
+    if (url.includes('localhost:300')) return false
+    if (url.includes('127.0.0.1:5501')) return false
+    return regex.test(url)
+  }
 
   const urlLinks = filteredUrls.map((url, idx) => (
     <ExternalLink key={idx} urlString={urlTable[url].link}>
-      {urlTable[url].text} <img src={share} alt="share" />
+      {isValidUrl(urlTable[url].link)
+        ? <>{urlTable[url].text} <img src={share} alt="share" /></>
+        : <>{urlTable[url].text} <img src={broken} alt="share" /></>
+      }
     </ExternalLink>
   ))
 
