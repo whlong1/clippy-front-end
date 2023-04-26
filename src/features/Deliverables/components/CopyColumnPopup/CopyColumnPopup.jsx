39e85effb31bf1d4db172498a3eefb7ba8d0a662
whlong1 <whunterlong@gmail.com>
2023-04-26 12:22:16 -0400

    Refactor handleCopy for nested usestate variable, clean up code

--- src/features/Deliverables/components/CopyColumnPopup/CopyColumnPopup.jsx
+++ src/features/Deliverables/components/CopyColumnPopup/CopyColumnPopup.jsx
@@ -1,35 +1,34 @@
+import { useState } from 'react'
+
+// Style
 import './CopyColumnPopup.css'
-import { useState, useEffect } from 'react'
+
+// Components
 import Popup from '../../../../layouts/Popup'
 
+// Helpers
 import { filterAndFormatColumns } from '../../helpers/helpers'
 
 const CopyColumnPopup = (props) => {
   const { id, isOpen, setIsOpen, deliverable } = props
   const filteredColumns = filterAndFormatColumns(deliverable)
-  const [copied, setCopied] = useState(filteredColumns.map((c) => false))
+  const initialState = filteredColumns.map((c) => false)
+  const [copied, setCopied] = useState(initialState)
 
   const statusTable = {
     missing: "Missing", complete: "Complete", incomplete: "Incomplete"
   }
 
-  useEffect(() => {
-    const resetCopy = () => { setTimeout(() => setCopied(false), 2000) }
-    if (copied) resetCopy()
-  }, [copied])
-
-  const handleCopy = (col) => {
+  const handleCopy = (col, idx) => {
     const deliverableRecord = deliverable.students.map((s) =>
       col === 'status'
         ? statusTable[s[col]] ? statusTable[s[col]] + "\n" : "\n"
         : s[col] ? s[col] + "\n" : "\n"
     )
     navigator.clipboard.writeText(deliverableRecord.join(""))
-    setCopied(true)
+    setCopied((prev) => prev.map((u, i) => i === idx ? true : false))
+    setTimeout(() => setCopied(initialState), 800)
   }
-
-  console.log('filter', filterAndFormatColumns(deliverable))
-  console.log('del', deliverable.students)
 
   return (
     <Popup key={id} isOpen={isOpen}>
@@ -43,7 +42,6 @@ const CopyColumnPopup = (props) => {
           </button>
         </section>
       </div>
-
       <div className="copyColumn">
         <header>
           <h1>Copy Options</h1>
@@ -55,11 +53,8 @@ const CopyColumnPopup = (props) => {
           {filteredColumns.map((col, idx) => (
             <div className="urlColumn" key={idx}>
               <p>{col.title} Column</p>
-              <button
-                onClick={() => handleCopy(col.field)}
-                style={{ margin: "0" }}>
+              <button onClick={() => handleCopy(col.field, idx)} style={{ margin: "0" }}>
                 {copied[idx] ? "COPIED" : "COPY"}
-                {/* COPY */}
               </button>
             </div>
           ))}
